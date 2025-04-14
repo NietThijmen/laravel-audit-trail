5d831e190e15f3221329173960c3b35a3929530e
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2025-04-14 12:07:54 +0200

    chore! observer try catch + causer_data to method

--- src/AuditableObserver.php
+++ src/AuditableObserver.php
@@ -2,25 +2,49 @@
 
 namespace NietThijmen\LaravelAuditTrail;
 
+use Illuminate\Support\Facades\Log;
 use NietThijmen\LaravelAuditTrail\Contracts\Auditable;
 use NietThijmen\LaravelAuditTrail\Models\AuditTrail;
 
 class AuditableObserver
 {
+
+    private function get_causer_data(Auditable $auditable)
+    {
+        if(auth()->user() !== null) {
+            return [
+                'causer' => get_class(auth()->user()),
+                'causer_id' => auth()->user()->id,
+            ];
+        }
+
+        return [
+            'causer' => null,
+            'causer_id' => null,
+        ];
+    }
+
     /**
      * Handle the User "created" event.
      */
     public function created(Auditable $auditable): void
     {
-        AuditTrail::create([
-            'action' => 'created',
-            'model' => get_class($auditable),
-            'model_id' => $auditable->id,
-            'causer' => get_class(auth()->user()),
-            'causer_id' => auth()->user()->id,
-            'old_data' => null,
-            'new_data' => $auditable->getAttributes(),
-        ]);
+        $causer_data = $this->get_causer_data($auditable);
+
+        try {
+            AuditTrail::create([
+                'action' => 'created',
+                'model' => get_class($auditable),
+                'model_id' => $auditable->id,
+                'causer' => $causer_data['causer'],
+                'causer_id' => $causer_data['causer_id'],
+                'old_data' => null,
+                'new_data' => $auditable->getAttributes(),
+            ]);
+        } catch (\Throwable $throwable) {
+            Log::debug("Error creating audit trail for model: " . get_class($auditable) . " with id: " . $auditable->id);
+        }
+
     }
 
     /**
@@ -28,15 +52,21 @@ class AuditableObserver
      */
     public function updated(Auditable $auditable): void
     {
-        AuditTrail::create([
-            'action' => 'updated',
-            'model' => get_class($auditable),
-            'model_id' => $auditable->id,
-            'causer' => get_class(auth()->user()),
-            'causer_id' => auth()->user()->id,
-            'old_data' => $auditable->getOriginal(),
-            'new_data' => $auditable->getAttributes(),
-        ]);
+        $causer_data = $this->get_causer_data($auditable);
+
+        try {
+            AuditTrail::create([
+                'action' => 'updated',
+                'model' => get_class($auditable),
+                'model_id' => $auditable->id,
+                'causer' => $causer_data['causer'],
+                'causer_id' => $causer_data['causer_id'],
+                'old_data' => $auditable->getOriginal(),
+                'new_data' => $auditable->getAttributes(),
+            ]);
+        } catch (\Throwable $throwable) {
+            Log::debug("Error creating audit trail for model: " . get_class($auditable) . " with id: " . $auditable->id);
+        }
     }
 
     /**
@@ -44,15 +74,21 @@ class AuditableObserver
      */
     public function deleted(Auditable $auditable): void
     {
-        AuditTrail::create([
-            'action' => 'deleted',
-            'model' => get_class($auditable),
-            'model_id' => $auditable->id,
-            'causer' => get_class(auth()->user()),
-            'causer_id' => auth()->user()->id,
-            'old_data' => $auditable->getAttributes(),
-            'new_data' => null,
-        ]);
+        $causer_data = $this->get_causer_data($auditable);
+
+        try {
+            AuditTrail::create([
+                'action' => 'deleted',
+                'model' => get_class($auditable),
+                'model_id' => $auditable->id,
+                'causer' => $causer_data['causer'],
+                'causer_id' => $causer_data['causer_id'],
+                'old_data' => $auditable->getAttributes(),
+                'new_data' => null,
+            ]);
+        } catch (\Throwable $throwable) {
+            Log::debug("Error creating audit trail for model: " . get_class($auditable) . " with id: " . $auditable->id);
+        }
     }
 
     /**
@@ -60,15 +96,21 @@ class AuditableObserver
      */
     public function restored(Auditable $auditable): void
     {
-        AuditTrail::create([
-            'action' => 'restored',
-            'model' => get_class($auditable),
-            'model_id' => $auditable->id,
-            'causer' => get_class(auth()->user()),
-            'causer_id' => auth()->user()->id,
-            'old_data' => null,
-            'new_data' => $auditable->getAttributes(),
-        ]);
+        $causer_data = $this->get_causer_data($auditable);
+
+        try {
+            AuditTrail::create([
+                'action' => 'restored',
+                'model' => get_class($auditable),
+                'model_id' => $auditable->id,
+                'causer' => $causer_data['causer'],
+                'causer_id' => $causer_data['causer_id'],
+                'old_data' => null,
+                'new_data' => $auditable->getAttributes(),
+            ]);
+        } catch (\Throwable $throwable) {
+            Log::debug("Error creating audit trail for model: " . get_class($auditable) . " with id: " . $auditable->id);
+        }
     }
 
     /**
@@ -76,14 +118,20 @@ class AuditableObserver
      */
     public function forceDeleted(Auditable $auditable): void
     {
-        AuditTrail::create([
-            'action' => 'forceDeleted',
-            'model' => get_class($auditable),
-            'model_id' => $auditable->id,
-            'causer' => get_class(auth()->user()),
-            'causer_id' => auth()->user()->id,
-            'old_data' => $auditable->getAttributes(),
-            'new_data' => null,
-        ]);
+        $causer_data = $this->get_causer_data($auditable);
+
+        try {
+            AuditTrail::create([
+                'action' => 'forceDeleted',
+                'model' => get_class($auditable),
+                'model_id' => $auditable->id,
+                'causer' => $causer_data['causer'],
+                'causer_id' => $causer_data['causer_id'],
+                'old_data' => $auditable->getAttributes(),
+                'new_data' => null,
+            ]);
+        } catch (\Throwable $throwable) {
+            Log::debug("Error creating audit trail for model: " . get_class($auditable) . " with id: " . $auditable->id);
+        }
     }
 }
